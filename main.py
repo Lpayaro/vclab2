0e5d4f215bdb7771db8e034a1d83679b0bad5673
Lorenzo <lpayaro@gmail.com>
2026-03-16 14:31:20 +0100

    Added source images

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 
 
 
+#Images URLs
+
+#Laica 1863
+#https://www.loc.gov/pictures/resource/prok.00336/?co=prok
+
+# San Nicolas 01368
+#https://www.loc.gov/pictures/collection/prok/item/2018680847/
+
+#Soldier 01317 
+#https://www.loc.gov/pictures/collection/prok/item/2018681235/
+
+def main():
+  
+  
+  pass
 
 if __name__ == "__main__":
-  pass
+  main()
